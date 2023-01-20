7d8945d5b966113e0f986f71f1f1a532babb492a
Zakhar Vashchenko <zaharvaschenko@gmail.com>
2023-01-20 22:15:23 +0100

    added Magazine realization

--- Magazine.cpp
+++ Magazine.cpp
@@ -5,3 +5,22 @@
 #include "Magazine.h"
 
 Magazine::Magazine(const string &name, const string &author, int year) : Publication(name, author, year) {}
+
+void Magazine::addArticle(const string &obj) {
+    articles.push_back(obj);
+}
+
+void Magazine::delArticle(int index) {
+    articles.erase(articles.begin()+index);
+}
+
+void Magazine::show() const {
+    Publication::show();
+    for (int i = 0; i < articles.size(); ++i) {
+        std::cout << i+1 << ". " << articles[i] << std::endl;
+    }
+}
+
+string Magazine::type() const {
+    return "Magazine";
+}

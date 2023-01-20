6c36cdaf8eb62370eca339ccfea60b85d6351832
Zakhar Vashchenko <zaharvaschenko@gmail.com>
2023-01-20 22:01:42 +0100

    added Book prototype

---
+++ Book.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Zakhar Vashchenko on 20.01.23.
+//
+
+#include "Book.h"
+
+Book::Book(const string &name, const string &author, int year, const string &resume) : Publication(name, author, year),
+                                                                                       resume(resume) {}
+
+const string &Book::getResume() const {
+    return resume;
+}
+
+void Book::setResume(const string &resume) {
+    Book::resume = resume;
+}
---
+++ Book.h
@@ -0,0 +1,24 @@
+//
+// Created by Zakhar Vashchenko on 20.01.23.
+//
+
+#ifndef HOMEWORK17_BOOK_H
+#define HOMEWORK17_BOOK_H
+
+
+#include "Publication.h"
+
+class Book : public Publication {
+    string resume;
+public:
+    Book(const string &name, const string &author, int year, const string &resume);
+
+    const string &getResume() const;
+    void setResume(const string &resume);
+
+    void show() const;
+    string type() const;
+};
+
+
+#endif //HOMEWORK17_BOOK_H
